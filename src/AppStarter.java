9fc0301dd72bd75855c33e59236c8c692ee4eb3e
mat <mathiassand02@gmail.com>
2024-11-20 22:16:37 +0100

    test

--- src/AppStarter.java
+++ src/AppStarter.java
@@ -8,6 +8,7 @@ public class AppStarter {
             frame.setSize(800, 600);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
+            
         });
     }
 }
