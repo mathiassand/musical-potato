c56c114ae596079bbbfedb5f0f76b9ce1e28137d
mat <mathiassand02@gmail.com>
2024-11-20 23:42:45 +0100

    Update gitIgnore and add flatLaf

--- src/AppStarter.java
+++ src/AppStarter.java
@@ -1,3 +1,4 @@
+import com.formdev.flatlaf.FlatDarculaLaf;
 import hmi.graphicslibrary.TransparentPanel;
 import hmi.graphicslibrary.layouts.KeyValueLayout;
 
@@ -5,25 +6,32 @@ import javax.swing.*;
 
 public class AppStarter {
     public static void main(String[] args) {
+        try {
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            throw new RuntimeException("Failed to initialize FlatLaf", e);
+        }
+
         SwingUtilities.invokeLater(() -> {
             JFrame frame = new JFrame();
             frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             frame.setSize(800, 600);
             frame.setLocationRelativeTo(null);
             frame.setVisible(true);
-
-            TransparentPanel panel = new TransparentPanel(new KeyValueLayout());
-            panel.add(new JLabel("Hello World!"));
-            panel.add(new JButton("Button"));
-            panel.add(new JLabel("Hello World!"));
-            panel.add(new JButton("Button"));
-            panel.add(new JLabel("Hello World!"));
-            panel.add(new JButton("Button"));
-            panel.add(new JLabel("Hello World!"));
-            panel.add(new JButton("Button"));
-            panel.add(new JLabel("Hello World!"));
-            panel.add(new JButton("Button"));
-            frame.setContentPane(panel);
+            EDT.invokeOnEDT(() -> {
+                TransparentPanel panel = new TransparentPanel(new KeyValueLayout());
+                panel.add(new JLabel("Hello World!"));
+                panel.add(new JButton("Button"));
+                panel.add(new JLabel("Hello World!"));
+                panel.add(new JButton("Button"));
+                panel.add(new JLabel("Hello World!"));
+                panel.add(new JButton("Button"));
+                panel.add(new JLabel("Hello World!"));
+                panel.add(new JButton("Button"));
+                panel.add(new JLabel("Hello World!"));
+                panel.add(new JButton("Button"));
+                frame.setContentPane(panel);
+            });
         });
     }
 }
---
+++ src/EDT.java
@@ -0,0 +1,38 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class EDT {
+    /**
+     * invokes a runnable (task) on the EDT thread
+     *
+     * @param task the task
+     */
+    public static void invokeOnEDT(Runnable task) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            task.run();
+        } else {
+            try {
+                SwingUtilities.invokeLater(task);
+            } catch (Exception e) {
+                throw new RuntimeException("Error executing on EDT", e);
+            }
+        }
+    }
+
+    /**
+     * invokes a runnable (task) on the EDT thread and waits for its completion
+     *
+     * @param task the task
+     */
+    public static void invokeAndWait(Runnable task) {
+        if (SwingUtilities.isEventDispatchThread()) {
+            task.run();
+        } else {
+            try {
+                SwingUtilities.invokeLater(task);
+            } catch (Exception e) {
+                throw new RuntimeException("Error executing on EDT", e);
+            }
+        }
+    }
+}
