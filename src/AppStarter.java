cd7823515fbb9e7ab6efa6185f6b9246540a094b
mat <mathiassand02@gmail.com>
2024-11-20 21:37:41 +0100

    initial commit

---
+++ src/AppStarter.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+public class AppStarter {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            JFrame frame = new JFrame();
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.setSize(800, 600);
+            frame.setLocationRelativeTo(null);
+            frame.setVisible(true);
+        });
+    }
+}
